6f126c98a1cee43c4c2f9da12503d5f0c3d706dd
zhangxi <zhangxi_1989@sina.com>
2018-01-16 10:48:48 +0800

    clear

--- JumpArduino/JumpArduino.ino
+++ JumpArduino/JumpArduino.ino
@@ -1,15 +1,11 @@
-
-#include <SoftwareSerial.h>
 #include <Servo.h> 
 
 Servo servo;
-//SoftwareSerial ble(30,31);
 
 
 void setup() {
   Serial3.begin(9600);
-  //ble.begin(9600);
-  digitalWrite(12,HIGH);
+
   servo.attach(9);
   servo.write(152);
 }
@@ -18,15 +14,7 @@ void setup() {
 String op = "";
 
 void loop() {
-  //servo.write(155);
- // return;
-//  
-   // servo.write(142);
-   // delay(700);
-   // servo.write(148);
-   // delay(700);
-   // return;
-  
+
  if( Serial3.available() > 0 ){
     op += char(Serial3.read());
     delay(3);
@@ -42,15 +30,10 @@ void loop() {
   if (action == "m")
   {
     int x      = op.substring(1,5).toInt();
-  
-    Serial3.println(x+20);
-    Serial3.println("begin");
-    Serial3.println(x);
 
     servo.write(155);
     delay(x);
     servo.write(152);
-    //ble.println("svgfinish");
   }
   op = "";
 }
